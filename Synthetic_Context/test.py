937ca4cf8d73d4ee65d6367f01b8ec2ddbdc283d
Rasmus <r.juul97@gmail.com>
2024-02-02 09:44:41 +0100

    test the different iterations, sizes, and model types on the real data

--- Synthetic_Context/test.py
+++ Synthetic_Context/test.py
@@ -1,55 +1,100 @@
+from src import _PATH_DATA, _PATH_MODELS, _PROJECT_ROOT
+from src.data.dataloaders import MetricDataset#, BugNISTDataModule
 from src.models.unet import UNet_pl
+from src.models.swin_unetr import SwinUNETR_pl as SwinUNETR
 import torch
+import torch._dynamo
+from glob import glob
+import pandas as pd
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 import tifffile
-import torch.nn.functional as F
+from skimage import measure
 import numpy as np
 import argparse
-from skimage.transform import resize
+
+def test(model, test_loader):
+    
+    color_dict = {1:[255,0,0],2:[0,255,0],3:[0,0,255],4:[255,255,0],5:[255,0,255],6:[0,255,255],7:[161,161,255],8:[171,128,84],9:[255,128,191],10:[135,89,179],11:[255,191,128],12:[0,85,0]}
+    accuracies = []
+    for k,(img,label) in enumerate(tqdm(test_loader, unit="batch")):
+        img = img.to("cuda")
+        with torch.no_grad():
+            preds = model(img);
+            preds_sm = preds.softmax(dim=1).cpu().numpy();
+    
+            class_props = []
+            for i in range(1, 13):  # For each class channel
+                props = measure.regionprops(
+            
+                    #                        v-- softmax channel for this class
+            
+                    measure.label(preds_sm[0, i] > 0.25)  # Threshold softmax probability at 0.25
+            
+                )
+            
+                props = [p for p in props if p.area > 5**3]  # Remove small connected components
+            
+                class_props.append(props)
+            
+            for i, props in enumerate(class_props):
+                # For every found connected component
+                for p in props:
+                    bb = p.bbox
+            
+                    # Sets the found connected component to the mean value of the connected component
+                    preds_sm[0,i+1,bb[0]:bb[3],bb[1]:bb[4],bb[2]:bb[5]][p.image] = preds_sm[0,i+1,bb[0]:bb[3],bb[1]:bb[4],bb[2]:bb[5]][p.image].mean()
+            
+            out = torch.Tensor(preds_sm).softmax(dim=1).argmax(dim=1).to(torch.uint8)
+            accuracies.append(sum(out[out != 0] == label[out != 0])/len(label[out != 0]))
+    accuracies = torch.Tensor(accuracies)
+    print(accuracies.mean())
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="test")
-    parser.add_argument(
-        "--n",
-        type=int,
-        default=0,
-    )
+    parser.add_argument("--size", type=str, default="5000", help="model param path")
+    parser.add_argument("--model", type=str, default="small", help="model type")
+    parser.add_argument("--version", type=str, default="v1", help="model type")
 
     args = parser.parse_args()
 
-    model = UNet_pl(
-        spatial_dims=3,
-        in_channels=1,
-        out_channels=13,
-        channels=(4, 8, 16, 32, 64),
-        strides=(2, 2, 2, 2),
-        lr=1e-4,
-    )
+    torch.set_float32_matmul_precision("medium")
 
-    model_path = "models/2023-09-20-2208/UNet-epoch=210.ckpt"
-    model.load_state_dict(
-        torch.load(model_path, map_location=None)["state_dict"], strict=True
-    )
+    test_loader = DataLoader(MetricDataset(),
+                         batch_size=1,
+                         num_workers=0)
+
+    if args.model == "small":
+        model = UNet_pl(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=13,
+            channels=(4, 8, 16, 32, 64),
+            strides=(2, 2, 2, 2),
+            lr=1,
+        )
+    elif args.model == "large":
+        model = UNet_pl(
+                spatial_dims=3,
+                in_channels=1,
+                out_channels=13,
+                channels=(16, 32, 64, 128, 256, 512),
+                strides=(2, 2, 2, 2, 2),
+                num_res_units = 3,
+            )
+    elif args.model == "swin":
+        model = SwinUNETR(img_size=(256,128,128), in_channels=1, out_channels=13, feature_size=24)
+
+    
+    
+    model_path = glob(f"models/{args.model}{args.size}{args.version}*/*.ckpt")[0]
+    model.load_state_dict(torch.load(model_path, map_location=None)['state_dict'], strict=True)
+        
+    torch._dynamo.config.suppress_errors = True
+    model = torch.compile(model)
+    model.eval();
     model = model.to("cuda")
-    model.eval()
 
-    i = args.n
-    img = tifffile.imread(f"data/mixed_crop/Mix 10/Mix 10_00{i}.tif")
-    # img = img[18:-18,21:-21,21:-21]
-    img = resize(img, (256, 128, 128))
-    img = np.uint8(np.clip(img, 0, 1) * 255)
-    # tifffile.imwrite(f"test_results/mix_test4/Mix_10_00{i}_resized.tif",img)
-    img = torch.Tensor(img)
-    img = img.unsqueeze(0)
-    img = img.unsqueeze(0)
-    img = img.to("cuda")
-
-    with torch.no_grad():
-        out = model(img)
-
-    logits = F.log_softmax(input=out, dim=1)
-    logits = logits.argmax(dim=1)
-
-    tifffile.imwrite(
-        f"test_results/mix_test5/Mix_10_00{i}_resized_labelled.tif",
-        logits.to(torch.uint8).cpu().detach().numpy(),
-    )
+    test(model,test_loader)
+    print(model_path)
